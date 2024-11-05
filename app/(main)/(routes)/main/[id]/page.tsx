7f2383ab376bd0aae6e7a6326c134d81430ce1d0
Rafaele <rafaele.sinaguglia@gmail.com>
2024-11-05 19:40:46 +0100

    je sais plus ce que jai fais

--- app/(main)/(routes)/main/[id]/page.tsx
+++ app/(main)/(routes)/main/[id]/page.tsx
@@ -1,10 +1,12 @@
 "use client"; // Ajoutez ceci si le composant est destiné à être côté client
 
 import Cover from "@/app/(main)/_components/Cover";
+import Editor from "@/app/(main)/_components/Editor";
 import { fetchDocumentId } from "@/app/(main)/_components/Navbar";
 import Toolbar from "@/app/(main)/_components/Toolbar";
 import { Document } from "@prisma/client";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery , useMutation, useQueryClient} from "@tanstack/react-query";
+import { useParams } from "next/navigation";
 
 interface DocumentIdPageProps {
   params: {
@@ -13,11 +15,44 @@ interface DocumentIdPageProps {
 }
 
 const DocumentPage = ({ params }: DocumentIdPageProps) => {
+    const queryClient = useQueryClient()
+    const param = useParams()
   const { data: doc, error, isLoading } = useQuery({
     queryKey: ["document", params.id],
     queryFn: () => fetchDocumentId(params.id), // Utilisez `params.id` ici
     enabled: !!params.id, // Exécute la requête uniquement si `id` est défini
+    staleTime: 0, // S'assure que les données sont actualisées à chaque navigation
   });
+
+  const update = useMutation({
+    mutationFn: async ({ id, content }) => {
+      const response = await fetch(`/api/documents/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({content}),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to update title");
+      }
+
+      return response.json();
+    },
+     onSuccess: (updatedData) => {
+        console.log("CONTENT UPDATED:", updatedData);
+          // Met à jour le cache immédiatement après la mutation
+        queryClient.setQueryData(["document", params.id], (oldData) => ({
+            ...oldData,
+            content: updatedData.content,
+          }));
+     },
+  });
+
+  const onChange = (content: string) =>{
+    update.mutate({id: param.id, content})
+  }
 
   if (isLoading) return <div>Chargement...</div>;
   if (error) return <div>Erreur lors de la récupération du document</div>;
@@ -29,6 +64,7 @@ const DocumentPage = ({ params }: DocumentIdPageProps) => {
        <Cover url={doc.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={doc} />
+        <Editor onChange={onChange} initialContent={doc.content} />
       </div>
     </div>
   );
---
+++ app/(main)/_components/Editor.tsx
@@ -0,0 +1,52 @@
+import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { useCreateBlockNote } from "@blocknote/react";
+ 
+import { useEdgeStore } from "@/lib/edgestore";
+import { PartialBlock } from "@blocknote/core";
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
+};
+
+const Editor = ({
+  onChange,
+  initialContent,
+  editable
+}: EditorProps) => {
+
+  const { edgestore } = useEdgeStore();
+
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({ 
+      file
+    });
+
+    return response.url;
+  }
+
+ 
+  const editor = useCreateBlockNote({
+    editable,
+    initialContent: initialContent ? JSON.parse(initialContent) : undefined,
+    uploadFile: handleUpload
+  });
+  
+
+  return (
+    <div>
+      <BlockNoteView
+        editor={editor}
+        onChange={() => {
+            onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
+          }}
+
+      />
+    </div>
+  )
+}
+
+export default Editor;
